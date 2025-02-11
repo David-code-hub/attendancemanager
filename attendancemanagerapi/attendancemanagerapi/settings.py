f02cf14184ea8a07dd4ffb1881430eb6a3323223
David <david@rlabs.org>
2025-02-11 22:47:08 +0200

    update: increase token expiration

--- attendancemanagerapi/attendancemanagerapi/settings.py
+++ attendancemanagerapi/attendancemanagerapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,6 +52,11 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",  
     ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),  
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),  
 }
 
 MIDDLEWARE = [
