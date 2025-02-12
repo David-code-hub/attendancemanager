ab25dbd5657cde9bb6b00eb358da34aac1aaf514
David <david@rlabs.org>
2025-02-12 22:31:01 +0200

    add: no data placeholder,logout btn

--- client/pages/home/index.vue
+++ client/pages/home/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="mt-10 grid grid-cols-4 items-center justify-center gap-5">
-    <SessionCard v-for="i in 4" />
+  <div class="mb-10 flex items-center justify-center gap-2">
+    <input class="input-field w-80" placeholder="Search sessions..." />
+    <button class="submit-button w-fit border border-black bg-white text-black">Search</button>
   </div>
+
+  <AppNoDataPlaceholder />
+
+  <!-- <div class="mt-10 grid grid-cols-4 items-center justify-center gap-5">
+    <SessionCard v-for="i in 1" />
+  </div> -->
 </template>
 
 <script setup lang="ts">
